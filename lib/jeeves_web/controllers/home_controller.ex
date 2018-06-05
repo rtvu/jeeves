2019b41f0ee4ef0f2c3574554a545f93be00f08b
rtvu <rtvu@users.noreply.github.com>
2018-06-05 16:01:17 +0000

    Implemented authentication using plugs

--- lib/jeeves_web/controllers/home_controller.ex
+++ lib/jeeves_web/controllers/home_controller.ex
@@ -1,6 +1,8 @@
 defmodule JeevesWeb.HomeController do
   use JeevesWeb, :controller
 
+  plug(JeevesWeb.Plugs.AuthenticateUser)
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
--- lib/jeeves_web/router.ex
+++ lib/jeeves_web/router.ex
@@ -7,6 +7,7 @@ defmodule JeevesWeb.Router do
     plug(:fetch_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+    plug(JeevesWeb.Plugs.SetCurrentUser)
   end
 
   pipeline :api do
