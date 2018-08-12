cb90dedc71fce7eb87da8daf5c2132a0cd3ae882
rtvu <rtvu87@gmail.com>
2018-08-12 07:48:59 +0000

    Added layout for server-file-selector.vue. Still need a lot of work for functionality.

--- assets/js/vue/server-file-selector.vue
+++ assets/js/vue/server-file-selector.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>PrintClient</h1>
+  <div class="row my-1">
+    <div class="col-3">
+      <button type="button" class="btn btn-sm btn-dark btn-block" @click="click">{{ resource }}</button>
+    </div>
+    <div class="col">
+      <input type="text" class="form-control form-control-sm" readonly v-model="value">
+    </div>
+  </div>
 </template>
+
+<script>
+  export default {
+    props: ["resource", "startPath"],
+    data () {
+      return {
+        value: ""
+      }
+    },
+    methods: {
+      click: function () {
+        this.value = this.value + "click"
+      }
+    }
+  }
+</script>
