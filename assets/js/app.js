a5b3445746a83402a20f8c4b251b1e3db3eebb76
rtvu <rtvu87@gmail.com>
2018-08-18 04:55:25 +0000

    Added printer-connection.vue layout. Still need to add js/elixir functionality.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,9 @@ import "phoenix_html"
 
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import printerConnection from './vue/printer-connection'
 import serverFileSelector from './vue/server-file-selector'
 
 Vue.use(BootstrapVue);
+Vue.component('printer-connection', printerConnection)
 Vue.component('server-file-selector', serverFileSelector)
---
+++ assets/js/vue/printer-connection.vue
@@ -0,0 +1,86 @@
+<template>
+    <form>
+      <div class="row my-1">
+        <div class="col-2">
+          <button type="button" :class="connectButtonClass" @click="clickConnectButton">{{ connectButtonTitle }}</button>
+        </div>
+        <div class="col">
+          <input type="text" class="form-control form-control-sm" :readonly="inputState" v-model="printer">
+        </div>
+        <div class="col-2">
+          <button type="button" :class="controlButtonClass" :disabled="controlButtonState" @click="clickControlButton">{{ controlButtonTitle }}</button>
+        </div>
+        <div>
+          {{ printer }}
+        </div>
+      </div>
+    </form>
+</template>
+
+<script>
+  export default {
+    props: {
+    },
+    data () {
+      return {
+        connected: false,
+        control: false,
+        printer: ""
+      }
+    },
+    computed: {
+      connectButtonTitle () {
+        if (this.connected) {
+          return "Disconnect"
+        } else {
+          return "Connect"
+        }
+      },
+      connectButtonClass () {
+        if (this.connected) {
+          return ["btn", "btn-sm", "btn-block", "btn-danger"]
+        } else {
+          return ["btn", "btn-sm", "btn-block", "btn-success"]
+        }
+      },
+      inputState () {
+        if (this.connected) {
+          return true
+        } else {
+          return false
+        }
+      },
+      controlButtonTitle () {
+        if (this.connected && this.control) {
+          return "Release Control"
+        } else {
+          return "Take Control"
+        }
+      },
+      controlButtonClass () {
+        if (this.control) {
+          return ["btn", "btn-sm", "btn-block", "btn-warning"]
+        } else {
+          return ["btn", "btn-sm", "btn-block", "btn-danger"]
+        }
+      },
+      controlButtonState () {
+        if (this.connected) {
+          return false
+        } else {
+          return true
+        }
+      }
+    },
+    created () {
+    },
+    methods: {
+      clickConnectButton () {
+        this.connected = !this.connected
+      },
+      clickControlButton () {
+        this.control = !this.control
+      }
+    }
+  }
+</script>
