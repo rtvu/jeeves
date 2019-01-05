f96d617f850db881f06e0e715e94f48e5793d6da
rtvu <rtvu87@gmail.com>
2019-01-05 07:44:01 +0000

    Edited home page to not show Apps dropdown.

--- lib/jeeves_web/controllers/home_controller.ex
+++ lib/jeeves_web/controllers/home_controller.ex
@@ -1,8 +1,6 @@
 defmodule JeevesWeb.HomeController do
   use JeevesWeb, :controller
 
-  plug(JeevesWeb.Plugs.AuthenticateUser)
-
   def index(conn, _params) do
     render(conn, "index.html")
   end
