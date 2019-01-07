034d1daace90889509638660ebd32f0e51e1901a
rtvu <rtvu87@gmail.com>
2019-01-07 00:26:49 +0000

    Added file structure for request-manager.vue and reenable Vue to work.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,15 @@ import "@fortawesome/fontawesome-free/js/all"
 import 'bootstrap'
 
 import Vue from 'vue'
+window.Vue = Vue
 //import BootstrapVue from 'bootstrap-vue'
 import printerConnection from './vue/printer-connection'
 import serverFileSelector from './vue/server-file-selector'
 import displayUsers from './vue/display-users'
+import requestManager from './vue/request-manager'
 
 //Vue.use(BootstrapVue);
 Vue.component('printer-connection', printerConnection)
 Vue.component('server-file-selector', serverFileSelector)
 Vue.component('display-users', displayUsers)
+Vue.component('request-manager', requestManager)
