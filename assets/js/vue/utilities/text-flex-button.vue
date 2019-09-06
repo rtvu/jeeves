b6ff72386f280be075f2dbc2469fb125b1bcd0a6
rtvu <rtvu87@gmail.com>
2019-09-06 15:19:21 +0000

    Partial save

--- assets/js/vue/utilities/text-flex-button.vue
+++ assets/js/vue/utilities/text-flex-button.vue
@@ -4,6 +4,7 @@
     style="overflow: hidden; white-space: nowrap;"
 
     ref="button"
+
     v-bind="$attrs"
     v-on="$listeners">
       <div style="display: inline-block;" ref="content"></div>
@@ -14,18 +15,66 @@
 
 <script>
   import $ from "jquery"
+  import Vue from "vue"
+  import { onMounted, onBeforeUnmount, ref, watch, computed } from "@vue/composition-api"
 
   export default {
     props: ["html"],
-    data () {
-      return {
+    setup(props, context) {
+      const model = reactive({
         ellipsis: "..",
         windowWidth: -1,
         buttonWidth: -1,
         htmlWidth: -1,
-        ellipsisWidth: -1
+        ellipsisWidth: -1,
+      })
+
+      function getWidths() {
+        model.buttonWidth = $(context.refs.button).width()
+        model.htmlWidth = $(context.refs.html).width()
+        model.ellipsisWidth = $(context.refs.ellipsis).width()
       }
-    },
+
+      onMounted(() => {
+        $(context.refs.html).html(props.html)
+
+        Vue.nextTick(() => {
+          // getWidths()
+
+          window.addEventListener("resize", () => {
+            windowWidth.value = window.innerWidth
+          })
+        })
+      })
+
+      onBeforeUnmount(() => {
+        window.removeEventListener("resize", () => {
+          windowWidth.value = window.innerWidth
+        })
+      })
+
+      watch(
+        () => props.html,
+        (html) => {
+          $(context.refs.html).html(html)
+
+          Vue.nextTick(() => {
+            getWidths()
+
+            Vue.nextTick(() => {
+              if (selector.value === "HTML") {
+                $(context.refs.content).html(html)
+              }
+            })
+          })
+        }
+      )
+
+
+
+      return {}
+    }
+
     computed: {
       selector () {
         if (this.windowWidth === this.buttonWidth && this.windowWidth === this.htmlWidth && this.windowWidth === this.ellipsisWidth && this.windowWidth === -1) {
@@ -54,40 +103,8 @@
 
         this.$emit("selector", {selector: newSelector})
       },
-      html (newHTML, oldHTML) {
-        $(this.$refs.html).html(newHTML)
 
-        this.$nextTick(() => {
-          this.getWidths()
-
-          if (this.selector === "HTML") {
-            $(this.$refs.content).html(this.html)
-          }
-        })
-      }
     },
-    mounted () {
-      $(this.$refs.html).html(this.html)
 
-      this.$nextTick(() => {
-        this.getWidths()
-
-        window.addEventListener("resize", () => {
-          this.windowWidth = window.innerWidth
-        })
-      })
-    },
-    beforeDestroy () {
-      window.removeEventListener("resize", () => {
-        this.windowWidth = window.innerWidth
-      })
-    },
-    methods: {
-      getWidths () {
-        this.buttonWidth = $(this.$refs.button).width()
-        this.htmlWidth = $(this.$refs.html).width()
-        this.ellipsisWidth = $(this.$refs.ellipsis).width()
-      }
-    }
   }
 </script>
