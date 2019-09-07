af26c4fee62e813a983e26ee810d9aa06eb9f4ba
rtvu <rtvu87@gmail.com>
2019-09-07 05:30:16 +0000

    text-flex-button has been updated

--- assets/js/vue/utilities/text-flex-button.vue
+++ assets/js/vue/utilities/text-flex-button.vue
@@ -9,102 +9,87 @@
     v-on="$listeners">
       <div style="display: inline-block;" ref="content"></div>
       <div style="visibility: hidden; display: inline-block; position: absolute;" ref="html"></div>
-      <div style="visibility: hidden; display: inline-block; position: absolute;" ref="ellipsis">{{ ellipsis }}</div>
+      <div style="visibility: hidden; display: inline-block; position: absolute;" ref="ellipsis">{{ model.ellipsis }}</div>
   </button>
 </template>
 
 <script>
   import $ from "jquery"
   import Vue from "vue"
-  import { onMounted, onBeforeUnmount, ref, watch, computed } from "@vue/composition-api"
+  import { onMounted, onBeforeUnmount, reactive, watch } from "@vue/composition-api"
 
   export default {
     props: ["html"],
     setup(props, context) {
       const model = reactive({
         ellipsis: "..",
-        windowWidth: -1,
         buttonWidth: -1,
         htmlWidth: -1,
         ellipsisWidth: -1,
+        selector: "None"
       })
 
       function getWidths() {
         model.buttonWidth = $(context.refs.button).width()
         model.htmlWidth = $(context.refs.html).width()
         model.ellipsisWidth = $(context.refs.ellipsis).width()
+
+        if (model.buttonWidth < model.ellipsisWidth) {
+          model.selector = "None"
+        } else if (model.buttonWidth > model.htmlWidth) {
+          model.selector = "HTML"
+        } else {
+          model.selector = "Ellipsis"
+        }
       }
 
       onMounted(() => {
-        $(context.refs.html).html(props.html)
-
         Vue.nextTick(() => {
-          // getWidths()
-
           window.addEventListener("resize", () => {
-            windowWidth.value = window.innerWidth
+            getWidths()
+          })
+
+          watch(
+            () => model.selector,
+            (selector) => {
+              if (selector === "None") {
+                $(context.refs.content).html("")
+              } else if (selector === "HTML") {
+                $(context.refs.content).html(props.html)
+              } else {
+                $(context.refs.content).html(model.ellipsis)
+              }
+
+              context.emit("selector", {selector: selector})
+            }
+          )
+
+          watch(
+            () => props.html,
+            (html) => {
+              $(context.refs.html).html(html)
+
+              Vue.nextTick(() => {
+                getWidths()
+              })
+            }
+          )
+
+          Vue.nextTick(() => {
+            getWidths()
           })
         })
       })
 
       onBeforeUnmount(() => {
         window.removeEventListener("resize", () => {
-          windowWidth.value = window.innerWidth
+          getWidths()
         })
       })
 
-      watch(
-        () => props.html,
-        (html) => {
-          $(context.refs.html).html(html)
-
-          Vue.nextTick(() => {
-            getWidths()
-
-            Vue.nextTick(() => {
-              if (selector.value === "HTML") {
-                $(context.refs.content).html(html)
-              }
-            })
-          })
-        }
-      )
-
-
-
-      return {}
-    }
-
-    computed: {
-      selector () {
-        if (this.windowWidth === this.buttonWidth && this.windowWidth === this.htmlWidth && this.windowWidth === this.ellipsisWidth && this.windowWidth === -1) {
-          return "Initializing"
-        } else if (this.buttonWidth < this.ellipsisWidth) {
-          return "None"
-        } else if (this.buttonWidth > this.htmlWidth) {
-          return "HTML"
-        } else {
-          return "Ellipsis"
-        }
+      return {
+        model
       }
-    },
-    watch: {
-      windowWidth (newWidth, oldWidth) {
-        this.getWidths()
-      },
-      selector (newSelector, oldSelector) {
-        if (newSelector === "None") {
-          $(this.$refs.content).html("")
-        } else if (newSelector === "HTML") {
-          $(this.$refs.content).html(this.html)
-        } else {
-          $(this.$refs.content).html("..")
-        }
-
-        this.$emit("selector", {selector: newSelector})
-      },
-
-    },
-
+    }
   }
 </script>
