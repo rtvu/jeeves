856c6313421a9a98b1022af76f5122312c28c45d
rtvu <rtvu@users.noreply.github.com>
2019-11-13 01:31:17 +0000

    Partial save

--- assets/js/vue/utilities/resizable-textarea.vue
+++ assets/js/vue/utilities/resizable-textarea.vue
@@ -1,3 +1,8 @@
+<!--
+  'resizable-textarea' is a wrapper around 'textarea' to sepcialize in fitting
+  the textarea's height around the inputted text.
+-->
+
 <template>
   <textarea
     :value="value"
@@ -6,24 +11,39 @@
 </template>
 
 <script>
+  import Vue from "vue"
+  import { computed, onMounted } from "@vue/composition-api"
+
   export default {
-    props: ["value"],
-    computed: {
-      listeners () {
-        return Object.assign({}, this.$listeners, {
-          input: () => {
-            this.$emit("input", event.target.value)
-            
-            this.$el.style.height = "auto"
-            this.$el.style.height = this.$el.scrollHeight + "px"
-          }
-        })
+    props: {
+      value: {
+        type: String,
+        default: ""
       }
     },
-    mounted () {
-      this.$nextTick(() => {
-        this.$el.setAttribute("style", "height: " + this.$el.scrollHeight + "px; overflow-y: hidden;")
+    setup(props, context) {
+      console.log(context)
+      const listeners = computed(() => {
+        return {}
+        // return Object.assign({}, this.$listeners, {
+        //   input: () => {
+        //     this.$emit("input", event.target.value)
+        //
+        //     this.$el.style.height = "auto"
+        //     this.$el.style.height = this.$el.scrollHeight + "px"
+        //   }
+        // })
       })
+
+      onMounted(() => {
+        Vue.nextTick(() => {
+          // this.$el.style.height = this.$el.scrollHeight + "px"
+          // this.$el.style.overflowY = "hidden"
+          // this.$el.style.resize = "none"
+        })
+      })
+
+      return { listeners }
     }
   }
 </script>
